4dcc81beb34cf6c6dac41d0aac4ab7fca3740273
Pablo-source <datavizvalencia@gmail.com>
2023-12-03 18:12:52 +0000

    added path in

--- 02_model.R
+++ 02_model.R
@@ -2,3 +2,11 @@
 
 library(bigml)
 library(tidyverse)
+
+# Load .csv file from objects folder in R
+
+path_in <- here("objects","dataset.csv")
+dataset <- read_csv(path_in, col_names = TRUE, col_types=  cols())
+
+head(dataset)
+
