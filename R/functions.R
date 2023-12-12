6205f8b045a088ddce4aa1bc3c37e196dbeb72de
Pablo-source <datavizvalencia@gmail.com>
2023-12-12 17:16:45 +0000

    Created helper script with functions for Targets pacakge

---
+++ R/functions.R
@@ -0,0 +1,11 @@
+# functions
+
+## Helper ---------------
+
+source_all <-function(path = "R"){
+  files <- list.files(here::here(path),
+                      full.names = TRUE,
+                      pattern = "R$")
+  suppressMessages(lapply(files,source))
+  invisible(path)
+}
